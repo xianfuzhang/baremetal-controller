b2ee06d9f24fccbe9a5c1a81aa1b8a905c963a5c
xianfu.zhang <286330850@qq.com>
2023-07-24 09:58:43 +0800

    设置update-codegen

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -26,8 +26,8 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
-  ./pkg/generated \
-  ./pkg/apis \
+  github.com/xianfuzhang/baremetal-controller/pkg/generated \
+  github.com/xianfuzhang/baremetal-controller/pkg/apis \
   baremetal:v1alpha1 \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
 
