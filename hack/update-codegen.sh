c69cdfecbe6b02418c498984eb0f150e5512ab80
xianfu.zhang <286330850@qq.com>
2023-07-24 10:05:54 +0800

    设置CRD和version以便生成代码

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -28,7 +28,7 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   ./pkg/generated \
   ./pkg/apis \
-  example:v1alpha1 \
+  baremetal:v1alpha1 \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
 
 # To use your own boilerplate text append:
